a962efe914bde136c8201b896d6958045a8ec8d0
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-22 11:24:23 +0700

    php

---
+++ php/variabel_konstanta.php
@@ -0,0 +1,16 @@
+<?php 
+$angka1 = 10;
+$angka2 = 5;
+$hasil = $angka1 + $angka2;
+echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.<br>";
+
+$benar = true;
+$salah = false;
+echo "Variabel benar: $benar, variabel salah: $salah <br>";
+
+define("NAMA_SITUS", "Websiteku.com");
+define("TAHUN_PENDAFTARAN", 2023);
+
+echo "Selamat datang di ". NAMA_SITUS .", situs yang
+didirikan pada tahun ". TAHUN_PENDAFTARAN .".";
+?>
