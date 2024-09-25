8c804644e74885def67ecd1c13857fb9f7acac0d
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-25 15:55:22 +0700

    j5

--- php/praktikPHP/string3.php
+++ php/praktikPHP/string3.php
@@ -1,13 +1,26 @@
-<?php 
+<!-- <?php 
 
-$pesan = "saya arek malang";
+// $pesan = "saya arek malang";
 
-$pesanPerKata = explode(" ", $pesan);
+// $pesanPerKata = explode(" ", $pesan);
 
-$pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
+// $pesanPerKata = array_map(fn($pesan) => strrev($pesan), $pesanPerKata);
 
-$pesan = implode(" ", $pesanPerKata);
+// $pesan = implode(" ", $pesanPerKata);
 
-echo $pesan . "<br>";
+// echo $pesan . "<br>";
 
-?>
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cara 01</title>
+</head>
+<body>
+    <p>Tanggal Hari ini: <?= date("d M Y")?></p>
+</body>
+</html>
+
