65b8421d0fbfc1f9ce99d73068dec253f26cdb32
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-09-25 15:04:34 +0700

    j5

--- php/praktikPHP/rekursif.php
+++ php/praktikPHP/rekursif.php
@@ -11,14 +11,63 @@
 //     echo "Perulangan ke-{$i} <br>";
 // }
 
-function tampilkanAngka (int $jumlah, int $indeks = 1){
-    echo "Perulangan ke-{$indeks} <br>";
+// function tampilkanAngka (int $jumlah, int $indeks = 1){
+//     echo "Perulangan ke-{$indeks} <br>";
 
-    if ($indeks < $jumlah) {
-        tampilkanAngka($jumlah, $indeks + 1);
+//     if ($indeks < $jumlah) {
+//         tampilkanAngka($jumlah, $indeks + 1);
+//     }
+// }
+
+// tampilkanAngka(20);
+
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [
+                        "nama" => "Pantai"
+                    ],
+                    [
+                        "nama" => "Gunung"
+                    ]
+                ]
+            ],
+            [
+                "nama" => "Kuliner"
+            ],
+            [
+                "nama" => "Hiburan"
+            ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ]
+];
+
+function tampilkanMenuBertingkat (array $menu) {
+    echo "<ul>";
+
+    foreach ($menu as $key => $item) {
+        echo "<li> {$item['nama']}";
+        if (isset($item['subMenu']) && array($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+         }
+         echo "</li>";
     }
+    echo "</ul>";
 }
 
-tampilkanAngka(20);
+tampilkanMenuBertingkat($menu);
 
 ?>
