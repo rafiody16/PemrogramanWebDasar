57060e97d1f2c1bdf0f2973127b7fbc0f2c86f6f
RAFI ODY PRASETYO <rafiody16@gmail.com>
2024-11-05 20:54:07 +0700

    php3

--- php3/form_multiupload.php
+++ php3/form_multiupload.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Unggah Dokumen</h2>
     <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="files[]" multiple="multiple" accept=".pdf, .doc, .docx">
+    <input type="file" name="files[]" multiple="multiple" accept=".png, .jpg, .jpeg, .gif">
     <input type="submit" value="Unggah">
     </form>
 </body>
